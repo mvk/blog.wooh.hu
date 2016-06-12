fc22b37c194eb0aa55cd4f36d366536b00dc803e
Adam Papai <wooh@wooh.hu>
2016-06-12 06:37:25 +0000

    Add c9 compatibility

--- start_dev.sh
+++ start_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hugo server --buildDrafts --theme=hugo_theme_robust
+hugo server --buildDrafts --theme=hugo_theme_robust --bind="0.0.0.0" --port="8080" --baseURL="cloudbees-wooh.c9users.io"
